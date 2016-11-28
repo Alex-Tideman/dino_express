65cd73b6954b3e2263adf0d8b772751afe843709
Alex Tideman <alex.tideman@gmail.com>
2016-11-27 17:36:28 -0700

    Remove nodemon

--- public/dinosaurs.js
+++ public/dinosaurs.js
@@ -7,4 +7,6 @@ var app = express()
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+app.get('/')
+
 module.exports = router
--- server.js
+++ server.js
@@ -5,5 +5,5 @@ var dinosaurs = require('./public/dinosaurs')
 app.use('/dinosaurs', dinosaurs)
 
 app.listen(3000, function () {
-  console.log('RrrarrrrRrrrr!')
+  console.log('RrrarrrrRrrrr server alive on port 3000')
 })
